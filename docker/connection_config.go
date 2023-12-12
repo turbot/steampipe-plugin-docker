7379fc4b6c826866954ffc1186275514422f590b
Madhushree Ray <madhushree@turbot.com>
2023-12-12 23:46:11 +0530

    Removed config schema

--- docker/connection_config.go
+++ docker/connection_config.go
@@ -2,44 +2,16 @@ package docker
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type dockerConfig struct {
-	Paths                  []string `cty:"paths" steampipe:"watch"`
-	DockerfilePaths        []string `cty:"dockerfile_paths" steampipe:"watch"`
-	DockerComposeFilePaths []string `cty:"docker_compose_file_paths" steampipe:"watch"`
-	Host                   *string  `cty:"host"`
-	APIVersion             *string  `cty:"api_version"`
-	CertPath               *string  `cty:"cert_path"`
-	TLSVerify              *bool    `cty:"tls_verify"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"paths": {
-		Type: schema.TypeList,
-		Elem: &schema.Attribute{Type: schema.TypeString},
-	},
-	"dockerfile_paths": {
-		Type: schema.TypeList,
-		Elem: &schema.Attribute{Type: schema.TypeString},
-	},
-	"docker_compose_file_paths": {
-		Type: schema.TypeList,
-		Elem: &schema.Attribute{Type: schema.TypeString},
-	},
-	"host": {
-		Type: schema.TypeString,
-	},
-	"api_version": {
-		Type: schema.TypeString,
-	},
-	"cert_path": {
-		Type: schema.TypeString,
-	},
-	"tls_verify": {
-		Type: schema.TypeBool,
-	},
+	Paths                  []string `hcl:"paths" steampipe:"watch"`
+	DockerfilePaths        []string `hcl:"dockerfile_paths" steampipe:"watch"`
+	DockerComposeFilePaths []string `hcl:"docker_compose_file_paths" steampipe:"watch"`
+	Host                   *string  `hcl:"host"`
+	APIVersion             *string  `hcl:"api_version"`
+	CertPath               *string  `hcl:"cert_path"`
+	TLSVerify              *bool    `hcl:"tls_verify"`
 }
 
 func ConfigInstance() interface{} {
